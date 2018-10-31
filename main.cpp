ed792b2b01010730221e9fb7a3778bb984223cf9
Jose <jrecinos@umail.ucsb.edu>
2018-10-31 13:01:56 -0700

    Implemented printing functions

--- main.cpp
+++ main.cpp
@@ -20,40 +20,40 @@ int main(int argc, char* arg[]){
     }
   
     switch(input){
-    case 1:
-      {
-	double amount=0;
-	std::string sender="";
-	std::string receiver="";
-	std::cout<<"\nInput the amount\n$";
-	std::cin>>amount;
-	std::cout<<"What is the name of the sender?\n";
-	std::cin>>sender;
-	std::cout<<"What is the name of the receiver?\n";
-	std::cin>>receiver;
-	Transactions->insert_transaction(amount, sender, receiver);
-	break;
-      }
-    case 2:
-      {
-	int input;
-	std::string name;
-	if(Transactions->getLatestTrans() == NULL){
-	  std::cout<<"\nNo transactions have been recorded yet\n"<<std::endl;
-	  break;
-	}
+      case 1:
+        {
+  	double amount=0;
+  	std::string sender="";
+  	std::string receiver="";
+  	std::cout<<"\nInput the amount\n$";
+  	std::cin>>amount;
+  	std::cout<<"What is the name of the sender?\n";
+  	std::cin>>sender;
+  	std::cout<<"What is the name of the receiver?\n";
+  	std::cin>>receiver;
+  	Transactions->insert_transaction(amount, sender, receiver);
+  	break;
+        }
+      case 2:
+        {
+  	int input;
+  	std::string name;
+  	if(Transactions->getLatestTrans() == NULL){
+  	  std::cout<<"\nNo transactions have been recorded yet\n"<<std::endl;
+  	  break;
+  	}
 
-	std::cout<<"Enter the sender's name\n";
-	std::cin>>name;
-	Transactions->find_transactions(name);
+  	std::cout<<"Enter the sender's name\n";
+  	std::cin>>name;
+  	Transactions->find_transactions(name);
 
-	break;
+  	break;
+        }
+      case 3:
+        bool valid= Transactions->verify_transactions();
+        std::cout<<valid<<std::endl;
+        break;
       }
-    case 3:
-      bool valid= Transactions->verify_transactions();
-      std::cout<<valid<<std::endl;
-      break;
-    }
   }
 }
 
